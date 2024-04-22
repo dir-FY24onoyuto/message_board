e62f378d8e0f9ac67bd2462c5c088f74f42c8ec3
dir-FY24onoyuto <yuto.ono.t380@dirbato.co.jp>
2024-04-22 16:12:32 +0900

    ページネーションを追加

--- message_board/src/models/Message.java
+++ message_board/src/models/Message.java
@@ -12,12 +12,18 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
 @Entity
+
 @NamedQueries({
     @NamedQuery(
-         name = "getAllMessages",
-         query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            ),
+    @NamedQuery(
+            name = "getMessagesCount",
+            query = "SELECT COUNT(m) FROM Message AS m"
             )
 })
+
 @Table(name= "messages")
 public class Message {
     @Id
